bc831233d3be75cc3901550264f22b57e0dbb6ef
Alex <tuaalexis@gmail.com>
2016-01-28 21:03:45 -0430

    model stores

--- core/sensor/models.py
+++ core/sensor/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from core.store.models import Departaments
 
-# Create your models here.
+
+class Sensors(models.Model):
+    sensor_id = models.IntegerField(primary_key=True, null=False)
+    sid = models.CharField(max_length=120)
+    model = models.CharField(max_length=30)
+    name = models.CharField(max_length=200)
+    mac = models.CharField(max_length=17)
+    call_home = models.BooleanField()
+    rate = models.DecimalField()
+    public_key = models.TextField()
+    essid = models.CharField(max_length=200)
+    register_time = models.DateTimeField()
+    sensor_ip = models.CharField(max_length=100)
+    departament = models.ForeignKey(Departaments, on_delete=models.CASCADE())
+
+    class Meta:
+        db_table = 'sensors'
--- core/store/models.py
+++ core/store/models.py
@@ -3,6 +3,46 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Cities(models.Model):
+    city_id = models.IntegerField(primary_key=True, null=False)
+    city = models.CharField(max_length=100)
+    country = models.ForeignKey(Countries, on_delete=models.CASCADE())
+
+    class Meta:
+        db_table = 'cities'
+
+
+class Countries(models.Model):
+    country_id = models.IntegerField(primary_key=True, null=False)
+    country = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'countries'
+
+
+class Departments(models.Model):
+    department_id = models.IntegerField(primary_key=True, null=False)
+    name = models.CharField(max_length=250)
+    store = models.ForeignKey(Stores, on_delete=models.CASCADE())
+
+    class Meta:
+        db_table = 'departments'
+
+
+class Locations(models.Model):
+    location_id = models.IntegerField(primary_key=True, null=False)
+    name = models.CharField(max_length=150)
+    address = models.CharField(200)
+    latitude = models.DecimalField(max_digits=8, decimal_places=5)
+    longitude = models.DecimalField(max_digits=8, decimal_places=5)
+    distance_threshold = models.DecimalField(max_digits=5, decimal_places=2)
+    city = models.ForeignKey(Cities, on_delete=models.CASCADE())
+    store = models.ForeignKey(Stores, on_delete=models.CASCADE())
+    
+    class Meta:
+        db_table = 'locations'
+
+
 # Models
 class Stores(models.Model):
     store_id = models.UUIDField(primary_key=True)
@@ -15,3 +55,4 @@ class Stores(models.Model):
 
     class Meta:
         db_table = 'stores'
+
