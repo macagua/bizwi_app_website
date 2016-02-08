c5b55cf029b01ef02bd26e4b973c7643fc8260c1
ThePhoenixBird <fenix.kun@gmail.com>
2016-02-07 23:08:11 -0430

    dropdown menu on top nav fix

--- docs/mockups/ui/js/scripts.js
+++ docs/mockups/ui/js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
   $('.button-collapse').sideNav();
   $('.collapsible').collapsible();
-  $('.dropdown-button').dropdown();
+  $('.dropdown-button').dropdown({
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      belowOrigin: true, // Displays dropdown below the button
+    });
   $('select').material_select();
   $('.modal-trigger').leanModal();
   $('table.display').DataTable();
