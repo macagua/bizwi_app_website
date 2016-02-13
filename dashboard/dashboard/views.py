7eeb49feda607f885257f34064b82faee64bb599
Javier León <jel1284@gmail.com>
2016-02-13 01:23:00 -0430

    fix dashboard

--- dashboard/dashboard/views.py
+++ dashboard/dashboard/views.py
@@ -428,7 +428,9 @@ def employees_list(request):
                  'customer_name': request.session.get("client_name")}
     if type(employees['data']) is not list:
         employees['data'] = []
-    return render(request, 'employees.html', employees)
+    return render(request, 'employees_admin.html', employees)
+
+
 
 
 def employee(request, id_employee=None):
@@ -436,7 +438,7 @@ def employee(request, id_employee=None):
     locations_list = get_stores(id_customer)
     info = {'customer_name': request.session.get("customer_name"),
             'languages': LANGUAGE_LIST,
-            'locations': locations_list}
+            'stores': stores}
 
     if request.method == 'POST':
 
@@ -449,7 +451,7 @@ def employee(request, id_employee=None):
                 'last_name': form.cleaned_data['last_name'],
                 'phone': form.cleaned_data['phone'],
                 'language': form.cleaned_data['language'],
-                'location': str(form.cleaned_data['location']),
+                'stores': str(form.cleaned_data['stores']),
                 'checkpass': str(form.cleaned_data['checkpass']),
                 'password': str(form.cleaned_data['password']),
             }
@@ -486,7 +488,7 @@ def employee(request, id_employee=None):
         else:
             info['page'] = "add"
 
-    return render(request, 'employee.html', info)
+    return render(request, 'employee_admin.html', info)
 
 
 def settings_employee(request):
@@ -612,3 +614,5 @@ def profile_edit(request):
     info['email'] = email
     #info.update(extra_info)
     return render(request, 'profile_form.html', info)
+
+
