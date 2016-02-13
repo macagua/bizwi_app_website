a35ba15d17fd3de9dbe914627b17f467d47066a4
ThePhoenixBird <fenix.kun@gmail.com>
2016-02-13 14:36:12 -0430

    Profiles page for client to update/complete his information

--- docs/mockups/ui/js/scripts.js
+++ docs/mockups/ui/js/scripts.js
@@ -5,12 +5,12 @@ $(document).ready(function(){
       constrain_width: false, // Does not change width of dropdown to that of the activator
       belowOrigin: true, // Displays dropdown below the button
     });
-  $('select').material_select();
   $('.modal-trigger').leanModal();
-  $('table.display').DataTable();
   $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year
-    format: 'dd/mm/yyyy' 
-  });
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year
+      formatSubmit: 'yyyy-mm-dd',
+      hiddenName: true
+    });
+  $('select').material_select();
 });
