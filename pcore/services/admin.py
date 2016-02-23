5ecca2bfa02f523fb91557d0455b0becd006624b
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-02-23 12:18:05 -0430

    TG-84 Added a custom behaviors for "Customers" Model from Django GUI Admin

--- pcore/services/admin.py
+++ pcore/services/admin.py
@@ -13,6 +13,16 @@ class BrandsAdmin(admin.ModelAdmin):
     list_display = ('customer', 'brand_name', 'is_active')
     list_filter = ['customer', 'brand_name', 'is_active']
 
+
+class CustomersAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Basic information', {'fields': ['customer_name', 'business_name', 'code_crm', 'prefix', 'description', 'telephone', 'email', 'url', 'age_range', 'is_active']}),
+        ('Social networks', {'fields': ['fb_fanpage', 'fb_merchant_id', 'twitter_id', 'instagram_id', 'google_id', 'gplus_id']}),
+        ('More details', {'fields': ['lang', 'timezone', 'locale', 'brand_enabled', 'gtin', 'creation_date', 'last_access', 'mod_date']}),
+    ]
+    list_display = ('code_crm', 'customer_name', 'business_name', 'telephone', 'email', 'url')
+    list_filter = ['customer_name', 'business_name', 'is_active']
+
 admin.site.register(BrandStyle)
 admin.site.register(BrandTags)
 admin.site.register(Brands, BrandsAdmin)
@@ -22,7 +32,7 @@ admin.site.register(Countries)
 admin.site.register(CustomerCategories)
 admin.site.register(CustomerStyles)
 admin.site.register(CustomerTags)
-admin.site.register(Customers)
+admin.site.register(Customers, CustomersAdmin)
 admin.site.register(Departments)
 admin.site.register(Districts)
 admin.site.register(LocationTags)
